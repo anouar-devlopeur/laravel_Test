5088e504589d84db236f09f6d2a67fb54a967d8f
anouar <anouar.ab95@gmail.com>
2024-01-28 01:44:28 +0100

    livewire update

--- example-app/resources/views/layout/master.blade.php
+++ example-app/resources/views/layout/master.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    @livewireStyles
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Styles -->
-
+@livewireStyles
 </head>
 
 <body class="antialiased">
--- example-app/resources/views/livewire/counter.blade.php
+++ example-app/resources/views/livewire/counter.blade.php
@@ -3,5 +3,5 @@
    <div style="text-align: center">
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
-</div>
+    </div>
 </div>
